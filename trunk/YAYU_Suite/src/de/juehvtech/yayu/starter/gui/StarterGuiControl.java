5d123a10e22f9104880d59a04bcc30de4edaa6d8
Jens Heuschkel <jens@jensheuschkel.de>
2013-01-22 16:42:05 +0100

    modyfi gitignore

---
+++ trunk/YAYU_Suite/src/de/juehvtech/yayu/starter/gui/StarterGuiControl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.juehvtech.yayu.starter.gui;
+
+/**
+ *
+ * @author Jens
+ */
+public class StarterGuiControl {
+    
+}
